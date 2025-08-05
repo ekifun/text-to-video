cd4c1b64a5f3c16e1f0347121c94302ec1e2435f
Chenghao Liu <john@macbook.attlocal.net>
2025-08-04 20:32:13 -0700

    fix(deploy): remove unsupported --buildkit flag from deploy_consumer.sh

--- deploy_consumer.sh
+++ deploy_consumer.sh
@@ -1,10 +1,7 @@
-# deploy_consumer.sh
-#!/bin/sh
-
-set -e
+#!/bin/bash
 
 echo "📦 Building consumer image..."
-sudo nerdctl build --buildkit=false -t text-to-video-consumer:latest -f Dockerfile.consumer .
+sudo nerdctl build -t text-to-video-consumer:latest -f Dockerfile.consumer .
 
 echo "💾 Saving image to tar archive..."
 sudo nerdctl save -o text-to-video-consumer.tar text-to-video-consumer:latest
