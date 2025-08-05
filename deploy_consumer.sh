f286cd6e52c5a376d7d600be3c074e6d2052cda3
Chenghao Liu <john@macbook.attlocal.net>
2025-08-04 18:21:29 -0700

    Move Dockerfile.consumer and deploy_consumer.sh to project root

--- deploy_consumer.sh
+++ deploy_consumer.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-# Move to project root (if not already there)
-cd "$(dirname "$0")/.."
+# Go to the script's directory
+cd "$(dirname "$0")"
+
+# Move to project root
+cd ..
 
 # Build the consumer image
 sudo nerdctl build -t text-to-video-consumer:latest -f Dockerfile.consumer .
 
-# Save and load into containerd
+# Save and import into containerd for Kubernetes
 sudo nerdctl save -o text-to-video-consumer.tar text-to-video-consumer:latest
 sudo ctr -n k8s.io images import text-to-video-consumer.tar
